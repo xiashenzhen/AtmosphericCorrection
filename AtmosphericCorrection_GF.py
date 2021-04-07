e2bc80cb982e2f998c77432e774e913170c8ad69
jason <jason@terraqt.com>
2021-04-07 16:18:04 +0800

    test GF

--- AtmosphericCorrection_GF.py
+++ AtmosphericCorrection_GF.py
@@ -5,118 +5,134 @@
 import glob
 import os
 import sys
-import tarfile            #解压缩
+import tarfile  # 解压缩
 import json
 import numpy as np
 import gdal
 import pdb
 import math
 import time
-import xml.dom.minidom    #读取xml格式的影像头文件
-from tqdm import tqdm     #进度条
+import xml.dom.minidom  # 读取xml格式的影像头文件
+from tqdm import tqdm  # 进度条
 from Py6S import *
 import argparse
 from base import MeanDEM
 
+
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--Input_dir',type=str,help='Input dir',default=None)
-    parser.add_argument('--Output_dir',type=str,help='Output dir',default=None)
+    parser.add_argument("--Input_dir", type=str, help="Input dir", default=None)
+    parser.add_argument("--Output_dir", type=str, help="Output dir", default=None)
 
     return parser.parse_args(argv)
 
+
 # 解压缩原始文件
 def untar(fname, dirs):
-    print("文件路径",fname)
+    print("文件路径", fname)
     try:
         t = tarfile.open(fname)
     except Exception as e:
         print("文件%s打开失败" % fname)
     t.extractall(path=dirs)
 
+
 def Block(IDataSet):
-    global cols,rows,atcfiles
-    #设置输出波段
+    global cols, rows, atcfiles
+    # 设置输出波段
     Driver = IDataSet.GetDriver()
     geoTransform1 = IDataSet.GetGeoTransform()
     ListgeoTransform1 = list(geoTransform1)
     ListgeoTransform1[5] = -ListgeoTransform1[5]
     newgeoTransform1 = tuple(ListgeoTransform1)
     proj1 = IDataSet.GetProjection()
-    OutRCname = os.path.join(atcfiles,outFileName+".tif")
-    outDataset = Driver.Create(OutRCname,cols,rows,4,gdal.GDT_Int32)
+    OutRCname = os.path.join(atcfiles, outFileName + ".tif")
+    outDataset = Driver.Create(OutRCname, cols, rows, 4, gdal.GDT_Int32)
     outDataset.SetGeoTransform(newgeoTransform1)
     outDataset.SetProjection(proj1)
-    #分别读取4个波段
-    for m in range(1,5):
+    # 分别读取4个波段
+    for m in range(1, 5):
         ReadBand = IDataSet.GetRasterBand(m)
         outband = outDataset.GetRasterBand(m)
         outband.SetNoDataValue(-9999)
-        #获取对应波段的增益gain和偏移bias
-        Gain,Bias = RadiometricCalibration(m)
+        # 获取对应波段的增益gain和偏移bias
+        Gain, Bias = RadiometricCalibration(m)
+        print(Gain, Bias)
 
-        #获取大气校正系数
+        # 获取大气校正系数
         AtcCofa, AtcCofb, AtcCofc = AtmosphericCorrection(m)
         nBlockSize = 1024
         i = 0
         j = 0
-        b = cols*rows
-        #进度条参数
-        XBlockcount = math.ceil(cols/nBlockSize)
-        YBlockcount = math.ceil(rows/nBlockSize)
-        print("第%d波段校正："%m)
+        b = cols * rows
+        # 进度条参数
+        XBlockcount = math.ceil(cols / nBlockSize)
+        YBlockcount = math.ceil(rows / nBlockSize)
+        print("第%d波段校正：" % m)
         try:
-            with tqdm(total=XBlockcount*YBlockcount,iterable='iterable',desc = '第%i波段:'%m) as pbar:
-            # with tqdm(total=XBlockcount*YBlockcount) as pbar:
+            with tqdm(
+                total=XBlockcount * YBlockcount, iterable="iterable", desc="第%i波段:" % m
+            ) as pbar:
+                # with tqdm(total=XBlockcount*YBlockcount) as pbar:
                 # print(pbar)
-                while i<rows:
-                    while j <cols:
-                        #保存分块大小
+                while i < rows:
+                    while j < cols:
+                        # 保存分块大小
                         nXBK = nBlockSize
                         nYBK = nBlockSize
 
-                        #最后不够分块的区域，有多少读取多少
-                        if i+nBlockSize>rows:
+                        # 最后不够分块的区域，有多少读取多少
+                        if i + nBlockSize > rows:
                             nYBK = rows - i
-                        if j+nBlockSize>cols:
-                            nXBK=cols - j
+                        if j + nBlockSize > cols:
+                            nXBK = cols - j
 
-                        #分块读取影像
-                        Image = ReadBand.ReadAsArray(j,i,nXBK,nYBK)
+                        # 分块读取影像
+                        Image = ReadBand.ReadAsArray(j, i, nXBK, nYBK)
 
-                        outImage =np.where(Image>0,Image*Gain + Bias,-9999)
+                        outImage = np.where(Image > 0, Image * Gain + Bias, -9999)
 
-                        y = np.where(outImage!=-9999,AtcCofa * outImage - AtcCofb,-9999)
-                        atcImage = np.where(y!=-9999,(y / (1 + y * AtcCofc))*10000,-9999)
+                        y = np.where(
+                            outImage != -9999, AtcCofa * outImage - AtcCofb, -9999
+                        )
+                        atcImage = np.where(
+                            y != -9999, (y / (1 + y * AtcCofc)) * 10000, -9999
+                        )
 
-                        outband.WriteArray(atcImage,j,i)
+                        outband.WriteArray(atcImage, j, i)
 
-                        j=j+nXBK
+                        j = j + nXBK
                         time.sleep(1)
                         pbar.update(1)
-                    j=0
-                    i=i+nYBK
+                    j = 0
+                    i = i + nYBK
         except KeyboardInterrupt:
             pbar.close()
             raise
         pbar.close()
 
-def RadiometricCalibration(BandId):
-    global cols,rows,SatelliteID,SensorID,Year,ImageType,config
-    if SensorID[0:3] == "WFV":
-        Gain_ =config["Parameter"][SatelliteID][SensorID][Year]["gain"][BandId-1]
-        Bias_ =config["Parameter"][SatelliteID][SensorID][Year]["offset"][BandId-1]
-    else:
-        Gain_ =config["Parameter"][SatelliteID][SensorID][Year][ImageType]["gain"][BandId-1]
-        Bias_ =config["Parameter"][SatelliteID][SensorID][Year][ImageType]["offset"][BandId-1]
 
-    return Gain_,Bias_
+def RadiometricCalibration(BandId):
+    global cols, rows, SatelliteID, SensorID, Year, ImageType, config
+    if SensorID[0:3] == "WFV":
+        Gain_ = config["Parameter"][SatelliteID][SensorID][Year]["gain"][BandId - 1]
+        Bias_ = config["Parameter"][SatelliteID][SensorID][Year]["offset"][BandId - 1]
+    else:
+        Gain_ = config["Parameter"][SatelliteID][SensorID][Year][ImageType]["gain"][
+            BandId - 1
+        ]
+        Bias_ = config["Parameter"][SatelliteID][SensorID][Year][ImageType]["offset"][
+            BandId - 1
+        ]
+
+    return Gain_, Bias_
+
 
 # 6s大气校正
 def AtmosphericCorrection(BandId):
-    global metedata,config,SatelliteID,SensorID
-    #读取头文件
+    global metedata, config, SatelliteID, SensorID
+    # 读取头文件
     dom = xml.dom.minidom.parse(metedata)
 
     # 6S模型
@@ -124,29 +140,42 @@ def AtmosphericCorrection(BandId):
 
     # 传感器类型 自定义
     s.geometry = Geometry.User()
-    s.geometry.solar_z = 90-float(dom.getElementsByTagName('SolarZenith')[0].firstChild.data)
-    s.geometry.solar_a = float(dom.getElementsByTagName('SolarAzimuth')[0].firstChild.data)
-    # s.geometry.view_z = float(dom.getElementsByTagName('SatelliteZenith')[0].firstChild.data)
-    # s.geometry.view_a = float(dom.getElementsByTagName('SatelliteAzimuth')[0].firstChild.data)
+    s.geometry.solar_z = 90 - float(
+        dom.getElementsByTagName("SolarZenith")[0].firstChild.data
+    )
+    s.geometry.solar_a = float(
+        dom.getElementsByTagName("SolarAzimuth")[0].firstChild.data
+    )
     s.geometry.view_z = 0
     s.geometry.view_a = 0
+
     # 日期
-    DateTimeparm = dom.getElementsByTagName('CenterTime')[0].firstChild.data
-    DateTime = DateTimeparm.split(' ')
-    Date = DateTime[0].split('-')
+    DateTimeparm = dom.getElementsByTagName("CenterTime")[0].firstChild.data
+    DateTime = DateTimeparm.split(" ")
+    Date = DateTime[0].split("-")
     s.geometry.month = int(Date[1])
     s.geometry.day = int(Date[2])
 
     # print(s.geometry)
     # 中心经纬度
-    TopLeftLat = float(dom.getElementsByTagName('TopLeftLatitude')[0].firstChild.data)
-    TopLeftLon = float(dom.getElementsByTagName('TopLeftLongitude')[0].firstChild.data)
-    TopRightLat = float(dom.getElementsByTagName('TopRightLatitude')[0].firstChild.data)
-    TopRightLon = float(dom.getElementsByTagName('TopRightLongitude')[0].firstChild.data)
-    BottomRightLat = float(dom.getElementsByTagName('BottomRightLatitude')[0].firstChild.data)
-    BottomRightLon = float(dom.getElementsByTagName('BottomRightLongitude')[0].firstChild.data)
-    BottomLeftLat = float(dom.getElementsByTagName('BottomLeftLatitude')[0].firstChild.data)
-    BottomLeftLon = float(dom.getElementsByTagName('BottomLeftLongitude')[0].firstChild.data)
+    TopLeftLat = float(dom.getElementsByTagName("TopLeftLatitude")[0].firstChild.data)
+    TopLeftLon = float(dom.getElementsByTagName("TopLeftLongitude")[0].firstChild.data)
+    TopRightLat = float(dom.getElementsByTagName("TopRightLatitude")[0].firstChild.data)
+    TopRightLon = float(
+        dom.getElementsByTagName("TopRightLongitude")[0].firstChild.data
+    )
+    BottomRightLat = float(
+        dom.getElementsByTagName("BottomRightLatitude")[0].firstChild.data
+    )
+    BottomRightLon = float(
+        dom.getElementsByTagName("BottomRightLongitude")[0].firstChild.data
+    )
+    BottomLeftLat = float(
+        dom.getElementsByTagName("BottomLeftLatitude")[0].firstChild.data
+    )
+    BottomLeftLon = float(
+        dom.getElementsByTagName("BottomLeftLongitude")[0].firstChild.data
+    )
 
     ImageCenterLat = (TopLeftLat + TopRightLat + BottomRightLat + BottomLeftLat) / 4
 
@@ -156,9 +185,13 @@ def AtmosphericCorrection(BandId):
 
     if ImageCenterLat > 15 and ImageCenterLat < 45:
         if s.geometry.month > 4 and s.geometry.month < 9:
-            s.atmos_profile = AtmosProfile.PredefinedType(AtmosProfile.MidlatitudeSummer)
+            s.atmos_profile = AtmosProfile.PredefinedType(
+                AtmosProfile.MidlatitudeSummer
+            )
         else:
-            s.atmos_profile = AtmosProfile.PredefinedType(AtmosProfile.MidlatitudeWinter)
+            s.atmos_profile = AtmosProfile.PredefinedType(
+                AtmosProfile.MidlatitudeWinter
+            )
 
     if ImageCenterLat > 45 and ImageCenterLat < 60:
         if s.geometry.month > 4 and s.geometry.month < 9:
@@ -178,10 +211,10 @@ def AtmosphericCorrection(BandId):
     # 通过研究去区的范围去求DEM高度。
     pointUL = dict()
     pointDR = dict()
-    pointUL["lat"] = max(TopLeftLat,TopRightLat,BottomRightLat,BottomLeftLat)
-    pointUL["lon"] = min(TopLeftLon,TopRightLon,BottomRightLon,BottomLeftLon)
-    pointDR["lat"] = min(TopLeftLat,TopRightLat,BottomRightLat,BottomLeftLat)
-    pointDR["lon"] = max(TopLeftLon,TopRightLon,BottomRightLon,BottomLeftLon)
+    pointUL["lat"] = max(TopLeftLat, TopRightLat, BottomRightLat, BottomLeftLat)
+    pointUL["lon"] = min(TopLeftLon, TopRightLon, BottomRightLon, BottomLeftLon)
+    pointDR["lat"] = min(TopLeftLat, TopRightLat, BottomRightLat, BottomLeftLat)
+    pointDR["lon"] = max(TopLeftLon, TopRightLon, BottomRightLon, BottomLeftLon)
     meanDEM = (MeanDEM(pointUL, pointDR)) * 0.001
 
     # 研究区海拔、卫星传感器轨道高度
@@ -192,21 +225,21 @@ def AtmosphericCorrection(BandId):
     # 校正波段（根据波段名称）
     if BandId == 1:
         SRFband = config["Parameter"][SatelliteID][SensorID]["SRF"]["1"]
-        s.wavelength = Wavelength(0.450,0.520,SRFband)
+        s.wavelength = Wavelength(0.450, 0.520, SRFband)
 
     elif BandId == 2:
         SRFband = config["Parameter"][SatelliteID][SensorID]["SRF"]["2"]
 
-        s.wavelength = Wavelength(0.520,0.590,SRFband)
+        s.wavelength = Wavelength(0.520, 0.590, SRFband)
 
     elif BandId == 3:
         SRFband = config["Parameter"][SatelliteID][SensorID]["SRF"]["3"]
 
-        s.wavelength = Wavelength(0.630,0.690,SRFband)
+        s.wavelength = Wavelength(0.630, 0.690, SRFband)
 
     elif BandId == 4:
         SRFband = config["Parameter"][SatelliteID][SensorID]["SRF"]["4"]
-        s.wavelength = Wavelength(0.770,0.890,SRFband)
+        s.wavelength = Wavelength(0.770, 0.890, SRFband)
 
     s.atmos_corr = AtmosCorr.AtmosCorrLambertianFromReflectance(-0.1)
 
@@ -215,59 +248,55 @@ def AtmosphericCorrection(BandId):
     xa = s.outputs.coef_xa
     xb = s.outputs.coef_xb
     xc = s.outputs.coef_xc
-    # x = s.outputs.values
     return (xa, xb, xc)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    from pprint import pprint
+
     script_path = os.path.split(os.path.realpath(__file__))[0]
-    #读取辐射校正和大气校正所需参数:增益、偏移和光谱响应函数
-    config_file = os.path.join(script_path,"RadiometricCorrectionParameter.json")
+    print(script_path)
+    # 读取辐射校正和大气校正所需参数:增益、偏移和光谱响应函数
+    config_file = os.path.join(script_path, "RadiometricCorrectionParameter.json")
     config = json.load(open(config_file))
 
-    #输入数据路径
-    InputFilePath = parse_arguments(sys.argv[1:]).Input_dir
-    OutputFilePath = parse_arguments(sys.argv[2:]).Output_dir
-
-    for root,dirs,tarFiles in os.walk(InputFilePath):
-        pass
-
+    InputFilePath = "/home/tq/vtmp/test_GF"
+    OutputFilePath = "/home/tq/vtmp/test_GF/L2"
+    tarFiles = [
+        "/home/tq/vtmp/test_GF/GF2_PMS1_E103.4_N38.3_20171111_L1A0002756999.tar.gz"
+    ]
     for tarFile in tarFiles:
-        print(tarFile)
         filename = os.path.basename(tarFile)
         fileType = filename[0:2]
         filename_split = filename.split("_")
-        if fileType == 'GF':
-            #GFType = filename[4:7]
+        if fileType == "GF":
             GFType = filename_split[1][:3]
-            intputname = os.path.join(InputFilePath,filename)
+            intputname = os.path.join(InputFilePath, filename)
             outFileName = filename[:-7]
-            outname = os.path.join(InputFilePath,outFileName)
-            atcfiles = os.path.join(OutputFilePath,outFileName)
+            outname = os.path.join(InputFilePath, outFileName)
+            atcfiles = os.path.join(OutputFilePath, outFileName)
+            print("文件" + filename + "开始解压缩")
 
-            print("文件"+filename+"开始解压缩")
+            # try:
+            #     untar(intputname, outname)
+            # except Exception as e:
+            #     continue
 
-            try:
-                untar(intputname, outname)
-            except Exception as e:
-                continue
-
-            if GFType == 'WFV':
-                # tiffFile = glob.glob(outname + "/*.tiff")[0]
-                # metedata = glob.glob(outname+"/*.xml")[0]
-                tiffFile = glob.glob(os.path.join(outname,"*.tiff"))[0]
-                metedata = glob.glob(os.path.join(outname,"*.xml"))[0]
-
-            elif GFType == 'PMS':
-                # tiffFile = glob.glob(outname + "/*mss*.tiff")[0]
-                # metedata = glob.glob(outname+"/*mss*.xml")[0]
-                tiffFile = glob.glob(os.path.join(outname,"*MSS*.tiff"))[0]
-                metedata = glob.glob(os.path.join(outname,"*MSS*.xml"))[0]
+            if GFType == "WFV":
+                tiffFile = glob.glob(os.path.join(outname, "*.tiff"))[0]
+                metedata = glob.glob(os.path.join(outname, "*.xml"))[0]
+            elif GFType == "PMS":
+                tiffFile = "/home/tq/vtmp/test_GF/GF2_PMS1_E103.4_N38.3_20171111_L1A0002756999/GF2_PMS1_E103.4_N38.3_20171111_L1A0002756999-MSS1.tiff"
+                # tiffFile = glob.glob(os.path.join(outname, "*MSS*.tiff"))[0]
+                metedata = glob.glob(os.path.join(outname, "*MSS*.xml"))[0]
+            else:
+                raise ("type error!")
 
             try:
                 os.mkdir(atcfiles)
             except Exception as e:
                 pass
-            print(filename+"解压缩完成")
+            print(filename + "解压缩完成")
 
             try:
                 IDataSet = gdal.Open(tiffFile)
@@ -276,14 +305,10 @@ if __name__ == '__main__':
 
             cols = IDataSet.RasterXSize
             rows = IDataSet.RasterYSize
-
-            # SatelliteID = filename[0:3]
-            # SensorID = filename[4:8]
-            # Year = filename[22:26]
             SatelliteID = filename_split[0]
             SensorID = filename_split[1]
             Year = filename_split[4][:4]
-            ImageType =os.path.basename(tiffFile)[-9:-6]
+            ImageType = os.path.basename(tiffFile)[-9:-6]
+            print(cols, rows, SatelliteID, SensorID, Year, ImageType)
 
             Block(IDataSet)
-
--- testpy6s.py
+++ testpy6s.py
@@ -6,6 +6,7 @@
 from Py6S import *
 import numpy as np
 
+
 def testpy6s():
     # 6S模型
     s = SixS()
@@ -21,11 +22,11 @@ def testpy6s():
     s.geometry.month = 3
     s.geometry.day = 27
 
-    #print(s.geometry)
+    # print(s.geometry)
 
     s.atmos_profile = AtmosProfile.PredefinedType(AtmosProfile.MidlatitudeWinter)
 
-    #print(s.atmos_profile)
+    # print(s.atmos_profile)
     # 气溶胶类型大陆
     s.aero_profile = AtmosProfile.PredefinedType(AeroProfile.Continental)
 
@@ -42,10 +43,43 @@ def testpy6s():
     s.altitudes.set_sensor_satellite_level()
 
     # 校正波段（根据波段名称）
-    s.wavelength = Wavelength(0.450,0.520,[0.26665045403732035, 0.3898905990356667, 0.4365734433632099, 0.46350262095778305, 0.49204568689798206, 0.5176199163843476, 0.540561115963024, 0.5682131273380822, 0.6002756993408136, 0.6221870897658232, 0.6305517292683029, 0.6391171618716124, 0.6629074375761411, 0.6941437481584642, 0.7198332777418507, 0.7410571188969691, 0.7656783699869292, 0.7967537861568459, 0.8253006271461241, 0.8405207257502543, 0.8417219636907287, 0.8447305755272894, 0.8699114324074595, 0.9136765723097032, 0.9508133004267264, 0.9819957569853817, 0.9946839532212262, 0.8918374093071325, 0.5940393710297492])
-                        
-    print(s.wavelength)
+    s.wavelength = Wavelength(
+        0.450,
+        0.520,
+        [
+            0.26665045403732035,
+            0.3898905990356667,
+            0.4365734433632099,
+            0.46350262095778305,
+            0.49204568689798206,
+            0.5176199163843476,
+            0.540561115963024,
+            0.5682131273380822,
+            0.6002756993408136,
+            0.6221870897658232,
+            0.6305517292683029,
+            0.6391171618716124,
+            0.6629074375761411,
+            0.6941437481584642,
+            0.7198332777418507,
+            0.7410571188969691,
+            0.7656783699869292,
+            0.7967537861568459,
+            0.8253006271461241,
+            0.8405207257502543,
+            0.8417219636907287,
+            0.8447305755272894,
+            0.8699114324074595,
+            0.9136765723097032,
+            0.9508133004267264,
+            0.9819957569853817,
+            0.9946839532212262,
+            0.8918374093071325,
+            0.5940393710297492,
+        ],
+    )
 
+    print(s.wavelength)
 
     # s.atmos_corr = AtmosCorr.AtmosCorrBRDFFromRadiance()
     # s.atmos_corr = AtmosCorr.AtmosCorrBRDFFromReflectance(15)
@@ -60,9 +94,10 @@ def testpy6s():
     xb = s.outputs.coef_xb
     xc = s.outputs.coef_xc
     x = s.outputs.values
-    print(xa,xb,xc)
+    print(xa, xb, xc)
     print(x)
     return (xa, xb, xc)
+
 
 def testL8py6s():
     # 6S模型
@@ -79,11 +114,11 @@ def testL8py6s():
     s.geometry.month = 11
     s.geometry.day = 1
 
-    #print(s.geometry)
+    # print(s.geometry)
 
     s.atmos_profile = AtmosProfile.PredefinedType(AtmosProfile.MidlatitudeWinter)
 
-    #print(s.atmos_profile)
+    # print(s.atmos_profile)
     # 气溶胶类型大陆
     s.aero_profile = AtmosProfile.PredefinedType(AeroProfile.Continental)
 
@@ -99,23 +134,51 @@ def testL8py6s():
     s.altitudes.set_sensor_satellite_level()
 
     # 校正波段（根据波段名称）
-    s.wavelength = Wavelength(0.436, 0.526,
-                      [1.00000000e-05, 1.79000000e-04, 4.55000000e-04,
-                                1.63350000e-03, 6.86900000e-03, 4.28880000e-02,
-                                2.71370000e-01, 7.90740500e-01, 9.03034000e-01,
-                                9.04677500e-01, 8.89667000e-01, 8.79232000e-01,
-                                8.79688000e-01, 8.89796500e-01, 8.48533000e-01,
-                                8.36270500e-01, 8.68497000e-01, 9.11461500e-01,
-                                9.31726000e-01, 9.54896500e-01, 9.56424000e-01,
-                                9.83834000e-01, 9.89469000e-01, 9.68066500e-01,
-                                9.88729000e-01, 9.61057500e-01, 9.66125000e-01,
-                                9.82077000e-01, 9.63135000e-01, 9.98249000e-01,
-                                8.44893000e-01, 1.19533500e-01, 5.32800000e-03,
-                                1.32850000e-03, 5.16000000e-04, 1.17000000e-04,
-                                2.30000000e-05])
+    s.wavelength = Wavelength(
+        0.436,
+        0.526,
+        [
+            1.00000000e-05,
+            1.79000000e-04,
+            4.55000000e-04,
+            1.63350000e-03,
+            6.86900000e-03,
+            4.28880000e-02,
+            2.71370000e-01,
+            7.90740500e-01,
+            9.03034000e-01,
+            9.04677500e-01,
+            8.89667000e-01,
+            8.79232000e-01,
+            8.79688000e-01,
+            8.89796500e-01,
+            8.48533000e-01,
+            8.36270500e-01,
+            8.68497000e-01,
+            9.11461500e-01,
+            9.31726000e-01,
+            9.54896500e-01,
+            9.56424000e-01,
+            9.83834000e-01,
+            9.89469000e-01,
+            9.68066500e-01,
+            9.88729000e-01,
+            9.61057500e-01,
+            9.66125000e-01,
+            9.82077000e-01,
+            9.63135000e-01,
+            9.98249000e-01,
+            8.44893000e-01,
+            1.19533500e-01,
+            5.32800000e-03,
+            1.32850000e-03,
+            5.16000000e-04,
+            1.17000000e-04,
+            2.30000000e-05,
+        ],
+    )
     # s.wavelength = Wavelength(PredefinedWavelengths.LANDSAT_OLI_B2)
     print(s.wavelength)
-
 
     # s.atmos_corr = AtmosCorr.AtmosCorrBRDFFromRadiance()
     # s.atmos_corr = AtmosCorr.AtmosCorrBRDFFromReflectance(15)
@@ -130,11 +193,11 @@ def testL8py6s():
     xb = s.outputs.coef_xb
     xc = s.outputs.coef_xc
     x = s.outputs.values
-    print(xa,xb,xc)
+    print(xa, xb, xc)
     print(x)
     return (xa, xb, xc)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # testpy6s()
     testL8py6s()
-
